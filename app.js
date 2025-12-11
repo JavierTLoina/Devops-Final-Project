1b6db9123713234164025d4095c45604554ef784
JaviDB <tejedajavier063@gmail.com>
2025-12-10 20:31:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hola Mundo desde DevOps Final Project!"); 
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hola Mundo desde DevOps Final Project!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
